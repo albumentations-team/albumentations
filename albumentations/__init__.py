72ff2574198edd6f2dcea78a61669d05f15af6f0
Alex Parinov <creafz@gmail.com>
2018-06-09 11:06:21 +0400

    Rename iaa module to imgaug

--- albumentations/__init__.py
+++ albumentations/__init__.py
@@ -2,4 +2,4 @@ from .core.composition import *
 from .core.transforms_interface import *
 from .augmentations.transforms import *
 from .detection.transforms import *
-from .iaa.transforms import *
+from .imgaug.transforms import *
