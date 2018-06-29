a948a34a570821c97b8e03b7d1d46382ce9d17cf
Alex Parinov <creafz@gmail.com>
2018-06-29 18:27:12 +0400

    Add test for ElasticTransform interpolation

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import pytest
 
-from albumentations import Transpose, Rotate, ShiftScaleRotate, OpticalDistortion, GridDistortion
+from albumentations import Transpose, Rotate, ShiftScaleRotate, OpticalDistortion, GridDistortion, ElasticTransform
 import albumentations.augmentations.functional as F
 
 
@@ -66,5 +66,24 @@ def test_grid_distortion_interpolation(interpolation):
                                        border_mode=cv2.BORDER_REFLECT_101)
     expected_mask = F.grid_distortion(mask, num_steps=1, xsteps=[1.3], ysteps=[1.3], interpolation=cv2.INTER_NEAREST,
                                       border_mode=cv2.BORDER_REFLECT_101)
+    assert np.array_equal(data['image'], expected_image)
+    assert np.array_equal(data['mask'], expected_mask)
+
+
+@pytest.mark.parametrize('interpolation', [cv2.INTER_NEAREST, cv2.INTER_LINEAR, cv2.INTER_CUBIC])
+def test_elastic_transform_interpolation(monkeypatch, interpolation):
+    image = np.random.randint(low=0, high=256, size=(100, 100, 3), dtype=np.uint8)
+    mask = np.random.randint(low=0, high=2, size=(100, 100), dtype=np.uint8)
+    monkeypatch.setattr('albumentations.augmentations.transforms.ElasticTransform.get_params',
+                        lambda *_: {'random_state': 1111})
+    aug = ElasticTransform(alpha=1, sigma=50, alpha_affine=50, interpolation=interpolation, p=1)
+    data = aug(image=image, mask=mask)
+    expected_image = F.elastic_transform_fast(image, alpha=1, sigma=50, alpha_affine=50, interpolation=interpolation,
+                                              border_mode=cv2.BORDER_REFLECT_101,
+                                              random_state=np.random.RandomState(1111))
+    expected_mask = F.elastic_transform_fast(mask, alpha=1, sigma=50, alpha_affine=50,
+                                             interpolation=cv2.INTER_NEAREST,
+                                             border_mode=cv2.BORDER_REFLECT_101,
+                                             random_state=np.random.RandomState(1111))
     assert np.array_equal(data['image'], expected_image)
     assert np.array_equal(data['mask'], expected_mask)
