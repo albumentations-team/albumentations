fec1f3bcf49b3c8ea12cd0a859908d271a1b7109
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2018-09-15 20:01:11 +0300

    Fix mask interpolation. Bug occured in transfomation classes which did not have named parameter 'interpolation' in apply(). Also discovered that IAA-based transformations IAAAffine, IAAPiecewiseAffine, IAAPerspective does not properly handle mask with values other than {0,1}

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -2,8 +2,9 @@ import numpy as np
 import cv2
 import pytest
 
-from albumentations import Transpose, Rotate, ShiftScaleRotate, OpticalDistortion, GridDistortion, ElasticTransform,\
-    VerticalFlip, HorizontalFlip
+from albumentations import Transpose, Rotate, ShiftScaleRotate, OpticalDistortion, GridDistortion, ElasticTransform, \
+    VerticalFlip, HorizontalFlip, RandomSizedCrop, RandomScale, IAAPiecewiseAffine, IAAAffine, IAAPerspective, \
+    LongestMaxSize
 import albumentations.augmentations.functional as F
 
 
@@ -88,3 +89,32 @@ def test_elastic_transform_interpolation(monkeypatch, interpolation):
                                              random_state=np.random.RandomState(1111))
     assert np.array_equal(data['image'], expected_image)
     assert np.array_equal(data['mask'], expected_mask)
+
+
+@pytest.mark.parametrize('transformation', [ElasticTransform(), GridDistortion(), ShiftScaleRotate(rotate_limit=45),
+                                            RandomScale(scale_limit=0.5), RandomSizedCrop((80, 90), 100, 100),
+                                            IAAAffine(scale=1.5), IAAPiecewiseAffine(scale=1.5), IAAPerspective(),
+                                            LongestMaxSize(50), Rotate()])
+def test_binary_mask_interpolation(transformation):
+    """ Checks whether transformations based on DualTransform does not introduce a mask interpolation artifacts"""
+    transformation.p = 1
+    image = np.random.randint(low=0, high=256, size=(100, 100, 3), dtype=np.uint8)
+    mask = np.random.randint(low=0, high=2, size=(100, 100), dtype=np.uint8)
+
+    data = transformation(image=image, mask=mask)
+    assert np.array_equal(np.unique(data['mask']), np.array([0, 1]))
+
+
+@pytest.mark.parametrize('transformation', [ElasticTransform(), GridDistortion(), ShiftScaleRotate(rotate_limit=45),
+                                            RandomScale(scale_limit=0.5), RandomSizedCrop((80, 90), 100, 100),
+                                            LongestMaxSize(50), Rotate()])
+def test_semantic_mask_interpolation(transformation):
+    """ Checks whether transformations based on DualTransform does not introduce a mask interpolation artifacts.
+    Note: IAAAffine, IAAPiecewiseAffine, IAAPerspective does not properly operate if mask has values other than {0;1}
+    """
+    transformation.p = 1
+    image = np.random.randint(low=0, high=256, size=(100, 100, 3), dtype=np.uint8)
+    mask = np.random.randint(low=0, high=4, size=(100, 100), dtype=np.uint8) * 64
+
+    data = transformation(image=image, mask=mask)
+    assert np.array_equal(np.unique(data['mask']), np.array([0, 64, 128, 192]))
