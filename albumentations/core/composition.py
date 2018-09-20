3e1ab409c95ba49fd53117ab04514388db2943d1
albu <al.buslaev@gmail.com>
2018-09-20 17:51:29 +0300

    Deprecate compose-with-bboxes, use compose instead

--- albumentations/core/composition.py
+++ albumentations/core/composition.py
@@ -7,89 +7,90 @@ from albumentations.augmentations.bbox_utils import convert_bboxes_from_albument
     convert_bboxes_to_albumentations, filter_bboxes
 
 
-__all__ = ['Compose', 'OneOf', 'OneOrOther', 'ComposeWithBoxes']
+__all__ = ['Compose', 'OneOf', 'OneOrOther']
 
 
 class Compose(object):
-    """Compose transforms together.
-
-    Args:
-        transforms (list): list of transformations to compose.
-        preprocessing_transforms (list): list of transforms to run before transforms
-        p (float): probability of applying all list of transforms. Default: 1.0.
-    """
-
-    def __init__(self, transforms, preprocessing_transforms=[], p=1.0):
-        self.preprocessing_transforms = preprocessing_transforms
-        self.transforms = [t for t in transforms if t is not None]
-        self.p = p
-
-    def __call__(self, **data):
-        need_to_run = random.random() < self.p
-        return self.run_transforms_if_needed(need_to_run, data)
-
-    def run_transforms_if_needed(self, need_to_run, data):
-        for t in self.preprocessing_transforms:
-            data = t(**data)
-        if need_to_run:
-            for t in self.transforms:
-                data = t(**data)
-        return data
-
-
-class ComposeWithBoxes(Compose):
     """Compose transforms and handle all transformations regrading bounding boxes
 
     Args:
         transforms (list): list of transformations to compose.
         preprocessing_transforms (list): list of transforms to run before transforms
-        bbox_format (str): format of bounding boxes. Should be 'coco' or 'pascal_voc'.
-        label_fields (list): list of fields that are joined with boxes, e.g labels. Should be same type as boxes.
-        min_area (float): minimum area of a bounding box. All bounding boxes whose visible area in pixels
-            is less than this value will be removed. Default: 0.0.
-        min_visibility (float): minimum fraction of area for a bounding box to remain this box in list. Default: 0.0.
+        postprocessing_transforms (list): list of transforms to run after transforms
+        bbox_params (dict):
+            format (str): format of bounding boxes. Should be 'coco' or 'pascal_voc'. If None - don't use bboxes.
+            label_fields (list): list of fields that are joined with boxes, e.g labels. Should be same type as boxes.
+            min_area (float): minimum area of a bounding box. All bounding boxes whose visible area in pixels
+                is less than this value will be removed. Default: 0.0.
+            min_visibility (float): minimum fraction of area for a bounding box to remain this box in list.
+                Default: 0.0.
         p (float): probability of applying all list of transforms. Default: 1.0.
     """
 
-    def __init__(self, transforms, bbox_format, label_fields=[], min_area=0., min_visibility=0.,
-                 preprocessing_transforms=[], p=1.0):
-        super(ComposeWithBoxes, self).__init__(transforms, preprocessing_transforms, p=p)
-        self.bbox_format = bbox_format
-        self.label_fields = label_fields
-        self.min_area = min_area
-        self.min_visibility = min_visibility
+    def __init__(self, transforms, preprocessing_transforms=[], postprocessing_transforms=[], bbox_params={}, p=1.0):
+        self.preprocessing_transforms = preprocessing_transforms
+        self.postprocessing_transforms = postprocessing_transforms
+        self.transforms = [t for t in transforms if t is not None]
+        self.p = p
+        self.bbox_format = bbox_params.get('format', None)
+        self.label_fields = bbox_params.get('label_fields', [])
+        self.min_area = bbox_params.get('min_area', 0.0)
+        self.min_visibility = bbox_params.get('min_visibility', 0.0)
 
     def __call__(self, **data):
         need_to_run = random.random() < self.p
         if self.preprocessing_transforms or need_to_run:
-            if 'bboxes' not in data:
-                raise Exception('Please name field with bounding boxes `bboxes`')
-            if self.label_fields:
-                for field in self.label_fields:
-                    bboxes_with_added_field = []
-                    for bbox, field_value in zip(data['bboxes'], data[field]):
-                        bboxes_with_added_field.append(list(bbox) + [field_value])
-                    data['bboxes'] = bboxes_with_added_field
-
-            rows, cols = data['image'].shape[:2]
-            data['bboxes'] = convert_bboxes_to_albumentations(data['bboxes'], self.bbox_format, rows, cols,
-                                                              check_validity=True)
+            if self.bbox_format is not None:
+                data = self.boxes_preprocessing(data)
 
             data = self.run_transforms_if_needed(need_to_run, data)
 
-            rows, cols = data['image'].shape[:2]
-            data['bboxes'] = filter_bboxes(data['bboxes'], rows, cols, self.min_area, self.min_visibility)
+            if self.bbox_format is not None:
+                data = self.boxes_postprocessing(data)
+        return data
 
-            data['bboxes'] = convert_bboxes_from_albumentations(data['bboxes'], self.bbox_format, rows, cols,
-                                                                check_validity=True)
+    def run_transforms_if_needed(self, need_to_run, data):
+        for t in self.preprocessing_transforms:
+            data = t(**data)
 
-            if self.label_fields:
-                for idx, field in enumerate(self.label_fields):
-                    field_values = []
-                    for bbox in data['bboxes']:
-                        field_values.append(bbox[4 + idx])
-                    data[field] = field_values
-                data['bboxes'] = [bbox[:4] for bbox in data['bboxes']]
+        if need_to_run:
+            for t in self.transforms:
+                data = t(**data)
+
+        for t in self.postprocessing_transforms:
+            data = t(**data)
+        return data
+
+    def boxes_preprocessing(self, data):
+        if 'bboxes' not in data:
+            raise Exception('Please name field with bounding boxes `bboxes`')
+        if self.label_fields:
+            for field in self.label_fields:
+                bboxes_with_added_field = []
+                for bbox, field_value in zip(data['bboxes'], data[field]):
+                    bboxes_with_added_field.append(list(bbox) + [field_value])
+                data['bboxes'] = bboxes_with_added_field
+
+        rows, cols = data['image'].shape[:2]
+        data['bboxes'] = convert_bboxes_to_albumentations(data['bboxes'], self.bbox_format, rows, cols,
+                                                          check_validity=True)
+
+        return data
+
+    def boxes_postprocessing(self, data):
+        rows, cols = data['image'].shape[:2]
+        data['bboxes'] = filter_bboxes(data['bboxes'], rows, cols, self.min_area, self.min_visibility)
+
+        data['bboxes'] = convert_bboxes_from_albumentations(data['bboxes'], self.bbox_format, rows, cols,
+                                                            check_validity=True)
+
+        if self.label_fields:
+            for idx, field in enumerate(self.label_fields):
+                field_values = []
+                for bbox in data['bboxes']:
+                    field_values.append(bbox[4 + idx])
+                data[field] = field_values
+            data['bboxes'] = [bbox[:4] for bbox in data['bboxes']]
         return data
 
 
