7217b1a6c5d3ecb4eed5e9ebef40f712407417ff
Alex Parinov <creafz@gmail.com>
2019-02-24 12:39:27 +0300

    Use Travis CI to check that transforms docs are not outdated (#201)

--- tools/make_transforms_docs.py
+++ tools/make_transforms_docs.py
@@ -1,6 +1,8 @@
 import inspect
+import os
 import sys
 from enum import Enum
+import argparse
 
 sys.path.append('..')
 import albumentations  # noqa: E402
@@ -16,6 +18,11 @@ IGNORED_CLASSES = {
 }
 
 
+READTHEDOCS_TEMPLATE = '[{name}](https://albumentations.readthedocs.io/en/latest/api/augmentations.html#albumentations'
+TRANSFORM_NAME_WITH_LINK_TEMPLATE = READTHEDOCS_TEMPLATE + '.augmentations.transforms.{name})'
+IMGAUG_TRANSFORM_NAME_WITH_LINK_TEMPLATE = READTHEDOCS_TEMPLATE + '.imgaug.transforms.{name})'
+
+
 class Targets(Enum):
     IMAGE = 'Image'
     MASKS = 'Masks'
@@ -23,9 +30,13 @@ class Targets(Enum):
     KEYPOINTS = 'Keypoints'
 
 
-READTHEDOCS_TEMPLATE = '[{name}](https://albumentations.readthedocs.io/en/latest/api/augmentations.html#albumentations'
-TRANSFORM_NAME_WITH_LINK_TEMPLATE = READTHEDOCS_TEMPLATE + '.augmentations.transforms.{name})'
-IMGAUG_TRANSFORM_NAME_WITH_LINK_TEMPLATE = READTHEDOCS_TEMPLATE + '.imgaug.transforms.{name})'
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='Commands', dest='command')
+    subparsers.add_parser('make')
+    check_parser = subparsers.add_parser('check')
+    check_parser.add_argument('filepath', type=str, help='Path to a file that should be checked')
+    return parser.parse_args()
 
 
 def make_separator(width, align_center):
@@ -106,15 +117,45 @@ def make_transforms_targets_links(transforms_info):
     )
 
 
-if __name__ == '__main__':
-    if __name__ == '__main__':
-        transforms_info = get_transforms_info()
-        image_only_transforms = {transform: info for transform, info in transforms_info.items() if info['image_only']}
-        dual_transforms = {transform: info for transform, info in transforms_info.items() if not info['image_only']}
-        image_only_transforms_links = make_transforms_targets_links(image_only_transforms)
-        dual_transforms_table = make_transforms_targets_table(
-            dual_transforms,
-            header=['Transform'] + [target.value for target in Targets],
+def check_docs(filepath, image_only_transforms_links, dual_transforms_table):
+    with open(args.filepath) as f:
+        text = f.read()
+    outdated_docs = []
+    if image_only_transforms_links not in text:
+        outdated_docs.append('Pixel-level')
+    if dual_transforms_table not in text:
+        outdated_docs.append('Spatial-level')
+    if not outdated_docs:
+        return
+
+    raise ValueError(
+        'Docs for the following transform types are outdated: {outdated_docs_headers}. '
+        'Generate new docs by executing the `python tools/{py_file} make` command '
+        'and paste them to {filename}.'.format(
+            outdated_docs_headers=', '.join(outdated_docs),
+            py_file=os.path.basename(os.path.realpath(__file__)),
+            filename=os.path.basename(filepath),
         )
+    )
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    command = args.command
+    if command not in {'make', 'check'}:
+        raise ValueError(
+            'You should provide a valid command: {{make|check}}. Got {command} instead.'.format(command=command)
+        )
+    transforms_info = get_transforms_info()
+    image_only_transforms = {transform: info for transform, info in transforms_info.items() if info['image_only']}
+    dual_transforms = {transform: info for transform, info in transforms_info.items() if not info['image_only']}
+    image_only_transforms_links = make_transforms_targets_links(image_only_transforms)
+    dual_transforms_table = make_transforms_targets_table(
+        dual_transforms,
+        header=['Transform'] + [target.value for target in Targets],
+    )
+    if command == 'make':
         print(image_only_transforms_links)
         print(dual_transforms_table)
+    else:
+        check_docs(args.filepath, image_only_transforms_links, dual_transforms_table)
