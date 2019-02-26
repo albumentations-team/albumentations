1a3e4eb1f58eefa6b1253c33392a6b10254d8017
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-02-26 21:17:30 +0200

    Added bias parameter to to_tuple

--- albumentations/core/transforms_interface.py
+++ albumentations/core/transforms_interface.py
@@ -5,15 +5,35 @@ import cv2
 __all__ = ['to_tuple', 'BasicTransform', 'DualTransform', 'ImageOnlyTransform', 'NoOp']
 
 
-def to_tuple(param, low=None):
-    if isinstance(param, (list, tuple)):
-        return tuple(param)
-    elif param is not None:
-        if low is None:
-            return -param, param
-        return (low, param) if low < param else (param, low)
-    else:
+def to_tuple(param, low=None, bias=None):
+    """ Converts input argument to min-max tuple
+    Args:
+        param (scalar, tuple or list of 2+ elements): Input value.
+            If value is scalar, return value would be (offset - value, offset + value).
+            If value is tuple, return value would be value + offset (broadcasted).
+        low:  Second element of tuple can be passed as optional argument
+        bias: An offset factor added to each element
+    """
+    if low is not None and bias is not None:
+        raise ValueError('Arguments low and bias are mutually exclusive')
+
+    if param is None:
         return param
+
+    if isinstance(param, (int, float)):
+        if low is None:
+            param = - param, + param
+        else:
+            param = (low, param) if low < param else (param, low)
+    elif isinstance(param, (list, tuple)):
+        param = tuple(param)
+    else:
+        raise ValueError('Argument param must be either scalar (int,float) or tuple')
+
+    if bias is not None:
+        return tuple([bias + x for x in param])
+
+    return tuple(param)
 
 
 class BasicTransform(object):
