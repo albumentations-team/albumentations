3f8d1ce2e67340d85e59a30b0e353be8d1b54bdb
Alexander Buslaev <al.buslaev@gmail.com>
2019-04-03 22:19:43 +0300

    Fix totensor torch (#246)
    * fix totensor in case of additional targets
    
    * Skip tests that require PyTorch and torchvision if those libraries are not installed. Install PyTorch and torchvision in CI environments.

--- tests/test_augmentations.py
+++ tests/test_augmentations.py
@@ -1,15 +1,6 @@
 import numpy as np
 import pytest
 
-try:
-    import torch
-    import torchvision
-    from albumentations.pytorch import ToTensor
-
-    torch_available = True
-except ImportError:
-    torch_available = False
-
 from albumentations import RandomCrop, PadIfNeeded, VerticalFlip, HorizontalFlip, Flip, Transpose, RandomRotate90, \
     Rotate, ShiftScaleRotate, CenterCrop, OpticalDistortion, GridDistortion, ElasticTransform, ToGray, RandomGamma, \
     JpegCompression, HueSaturationValue, RGBShift, Blur, MotionBlur, MedianBlur, \
@@ -141,14 +132,6 @@ def test_imgaug_dual_augmentations(augmentation_cls, image, mask):
     data = aug(image=image, mask=mask)
     assert data['image'].dtype == np.uint8
     assert data['mask'].dtype == np.uint8
-
-
-@pytest.mark.skipif(not torch_available, reason='PyTorch and torchvision are not available')
-def test_torch_to_tensor_augmentations(image, mask):
-    aug = ToTensor()
-    data = aug(image=image, mask=mask, force_apply=True)
-    assert data['image'].dtype == torch.float32
-    assert data['mask'].dtype == torch.float32
 
 
 @pytest.mark.parametrize(['augmentation_cls', 'params'], [
