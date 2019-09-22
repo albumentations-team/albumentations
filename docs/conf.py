a0a5802d166ad285a9e65203398dee8de4945605
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2019-09-22 10:48:58 -0700

    Added pre-commit hook (#366)
    * Added pre-commit hook
    
    * fix in commit instruction
    
    * typo
    
    * Better wording in readme
    
    * Fix infinity while loop in RandomFog #361 (#362)
    
    * Fixed maybe_process_in_chunks and added test for it (#364)
    
    * Fix RandomSizedBBoxSafeCrop. (#359)
    
    * Fix RandomSizedBBoxSafeCrop.
    
    Currently, even though 'bboxes' is given,
    'get_params_dependent_on_targets' method cannot return proper params,
    because 'targets_as_params' does not include 'bboxes'.
    
    So, I add 'bboxes' to the 'targets_as_params'.
    Now, 'bboxes' have to be given to run RandomSizedBBoxSafeCrop.
    But an empty list is allowed.
    
    * Add albumentations_bboxes to the test codes.
    
    * Assert when params don't include self.targets_as_params.
    
    * better wording in Readme
    
    * isort => blacl. Fix in docs
    
    * name fix in docs
    
    * fix in names
    
    * fix in names
    
    * typo
    
    * fix in name
    
    * better wording for contributing
    
    * Added config for black
    
    * Added config for black
    
    * Added missed comma
    
    * Removed copy paste with author names
    
    * Added explicit recommendation to install black and flake8
    
    * Removed comparisons of some transforms with Pillow.ImageOps (#370)
    
    * Dtypofix
    
    * Added pre-commit hook
    
    * fix in commit instruction
    
    * typo
    
    * Better wording in readme
    
    * better wording in Readme
    
    * isort => blacl. Fix in docs
    
    * name fix in docs
    
    * fix in names
    
    * fix in names
    
    * typo
    
    * fix in name
    
    * better wording for contributing
    
    * Added config for black
    
    * Added config for black
    
    * Added missed comma
    
    * Removed copy paste with author names
    
    * Added explicit recommendation to install black and flake8
    
    * Dtypofix

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,8 @@ import mock
 import re
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 
 try:
@@ -20,7 +21,7 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ['pytorch', 'torchvision.transforms', 'torchvision.transforms.functional']
+MOCK_MODULES = ["pytorch", "torchvision.transforms", "torchvision.transforms.functional"]
 for module_name in MOCK_MODULES:
     sys.modules[module_name] = Mock()
 
@@ -28,7 +29,7 @@ for module_name in MOCK_MODULES:
 def get_version():
     current_dir = os.path.abspath(os.path.dirname(__file__))
     root = os.path.dirname(current_dir)
-    version_file = os.path.join(root, 'albumentations', '__init__.py')
+    version_file = os.path.join(root, "albumentations", "__init__.py")
     with open(version_file) as f:
         return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
@@ -42,31 +43,26 @@ def get_version():
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.mathjax", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'albumentations'
-copyright = ('{}, Buslaev Alexander, Alex Parinov, Vladimir Iglovikov, '
-             'Evegene Khvedchenya', 'Druzhinin Mikhail').format(datetime.datetime.now().year)
-author = 'Buslaev Alexander, Alex Parinov, Vladimir Iglovikov, Evegene Khvedchenya, ' \
-         'Druzhinin Mikhail'
+project = "albumentations"
+
+authors = "Alexander Buslaev, Alex Parinov, Vladimir Iglovikov, Evegene Khvedchenya, Mikhail Druzhinin"
+
+copyright = ("{}, {}".format(datetime.datetime.now().year, authors),)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -87,10 +83,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -101,24 +97,24 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'collapse_navigation': True}
+html_theme_options = {"collapse_navigation": True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'albumentationsdoc'
+htmlhelp_basename = "albumentationsdoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -127,15 +123,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -144,20 +137,14 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'albumentations.tex', 'albumentations Documentation',
-     'Buslaev Alexander, Alexander Parinov, Vladimir Iglovikov', 'manual'),
-]
+latex_documents = [(master_doc, "albumentations.tex", "albumentations Documentation", authors, "manual")]
 
 
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'albumentations', 'albumentations Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "albumentations", "albumentations Documentation", [authors], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -166,7 +153,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'albumentations', 'albumentations Documentation',
-     author, 'albumentations', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "albumentations",
+        "albumentations Documentation",
+        authors,
+        "albumentations",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
