a4905a94e17fd72555feb2ad902b418f835a5cfc
Arseny Kravchenko <me@arseny.info>
2019-09-22 22:50:53 +0300

    downscale transform

--- albumentations/augmentations/functional.py
+++ albumentations/augmentations/functional.py
@@ -1315,6 +1315,20 @@ def to_gray(img):
     return cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
 
 
+@preserve_shape
+def downscale(img, scale, interpolation=cv2.INTER_NEAREST):
+    h, w, *_ = img.shape
+
+    need_cast = interpolation != cv2.INTER_NEAREST and img.dtype == np.uint8
+    if need_cast:
+        img = to_float(img)
+    downscaled = cv2.resize(img, None, fx=scale, fy=scale, interpolation=interpolation)
+    upscaled = cv2.resize(downscaled, (w, h), interpolation=interpolation)
+    if need_cast:
+        upscaled = from_float(np.clip(upscaled, 0, 1), dtype=np.dtype("uint8"))
+    return upscaled
+
+
 def to_float(img, max_value=None):
     if max_value is None:
         try:
