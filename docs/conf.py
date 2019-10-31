ab04b32dc62e724f8f699923c933ac946049f895
Alex Parinov <creafz@gmail.com>
2019-10-31 09:22:56 +0300

    Add missing template variable to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"collapse_navigation": True}
+html_theme_options = {"collapse_navigation": True, "theme_navigation_depth": 4}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
