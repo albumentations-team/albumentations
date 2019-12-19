ac2263b198e314acda69e01c53cca8ef56e5df64
Druzhinin <dipetm@gmail.com>
2019-12-19 18:39:51 +0300

    Optimize solarize

--- albumentations/pytorch/augmentations/image_only/functional.py
+++ albumentations/pytorch/augmentations/image_only/functional.py
@@ -357,11 +357,7 @@ def shift_hsv(img, hue_shift, sat_shift, val_shift):
 def solarize(img, threshold=128):
     dtype = img.dtype
     max_val = MAX_VALUES_BY_DTYPE[dtype]
-
-    result_img = img.clone()
-    cond = img >= threshold
-    result_img[cond] = max_val - result_img[cond]
-    return result_img
+    return torch.where(img >= threshold, max_val - img, img)
 
 
 @clipped
