fd97ed61ee0cf6a984ca65635ac929265138a212
Druzhinin <dipetm@gmail.com>
2020-01-28 14:43:29 +0300

    Using mask and bboxes in benchmark

--- albumentations/pytorch/utils/gpu_benchmark.py
+++ albumentations/pytorch/utils/gpu_benchmark.py
@@ -9,10 +9,10 @@ import numpy as np
 from tqdm import tqdm
 
 
-def _bench(n, img, transform, desc, file):
+def _bench(n, img, mask, bboxes, transform, desc, file):
     s = time.time()
     for _ in tqdm(range(n), desc=desc, file=file):
-        transform(image=img)
+        transform(image=img, bboxes=bboxes, mask=mask)
     e = time.time()
     return n / (e - s)
 
@@ -56,6 +56,20 @@ def print_results(results):
     print(results_to_pretty_string(results))
 
 
+def _gen_random_bboxes(shape):
+    h, w = shape[:2]
+    bboxes = []
+
+    for _ in range(np.random.randint(1, 100)):
+        x1 = np.random.randint(0, w - 1)
+        y1 = np.random.randint(0, h - 1)
+        x2 = np.random.randint(x1, w)
+        y2 = np.random.randint(y1, h)
+        bboxes.append([x1, y1, x2, y2, np.random.randint(100)])
+
+    return bboxes
+
+
 def gpu_benchmark(
     cpu_aug, gpu_aug, shapes, iterations, to_float_cpu=False, to_float_gpu=True, on_gpu_img=False, silent=False
 ):
@@ -95,13 +109,20 @@ def gpu_benchmark(
         stdout = sys.stdout
         sys.stdout = open(os.devnull, "w")
 
+    cpu_aug.p = 1
+    gpu_aug.p = 1
+    cpu_aug = A.Compose(cpu_aug, bbox_params=A.BboxParams("pascal_voc"))
+    gpu_aug = A.Compose(gpu_aug, bbox_params=A.BboxParams("pascal_voc"))
+
     print("Warming up: ", end="")
     s = time.time()
     if on_gpu_img:
         image = torch.rand([3, 100, 100], dtype=torch.float32, device="cuda")
+        mask = torch.rand([100, 100], dtype=torch.float32, device="cuda")
     else:
         image = np.random.random([100, 100, 3]).astype(np.float32)
-    gpu_aug(image=image)
+        mask = np.random.random([100, 100]).astype(np.float32)
+    gpu_aug(image=image, mask=mask, bboxes=_gen_random_bboxes([100, 100]))
     print(time.time() - s, end="\n\n")
 
     if not hasattr(iterations, "__len__"):
@@ -115,21 +136,33 @@ def gpu_benchmark(
     for n, shape in zip(iterations, shapes):
         cpu_image = np.random.randint(0, 256, shape, dtype=np.uint8)
         gpu_image = cpu_image.copy()
+        bboxes = _gen_random_bboxes(cpu_image.shape)
+        cpu_mask = np.random.randint(0, 2, shape[:2], dtype=np.uint8)
+        gpu_mask = cpu_mask.copy()
+
         if on_gpu_img:
             if len(gpu_image.shape) < 3:
                 gpu_image.reshape(gpu_image.shape + (1,))
             gpu_image = torch.from_numpy(gpu_image.transpose(2, 0, 1)).to("cuda")
+            gpu_mask = torch.from_numpy(gpu_mask).to("cuda")
 
         if to_float_cpu and cpu_image.dtype == np.uint8:
             cpu_image = cpu_image.astype(np.float32) / 255.0
+            cpu_mask = cpu_mask.astype(np.float32)
         if to_float_gpu:
             if isinstance(gpu_image, np.ndarray) and gpu_image.dtype == np.uint8:
                 gpu_image = gpu_image.astype(np.float32) / 255.0
+                gpu_mask = gpu_mask.astype(np.float32)
             elif gpu_image.dtype == torch.uint8:
                 gpu_image = gpu_image.float() / 255.0
+                gpu_mask = gpu_mask.float()
 
-        cpu_fps = _bench(n, cpu_image, cpu_aug, "CPU transforms. Shape: {}".format(shape), sys.stdout)
-        gpu_fps = _bench(n, gpu_image, gpu_aug, "GPU transforms. Shape: {}".format(shape), sys.stdout)
+        cpu_fps = _bench(
+            n, cpu_image, cpu_mask, bboxes, cpu_aug, "CPU transforms. Shape: {}".format(shape), sys.stdout
+        )
+        gpu_fps = _bench(
+            n, gpu_image, gpu_mask, bboxes, gpu_aug, "GPU transforms. Shape: {}".format(shape), sys.stdout
+        )
 
         results.append({"shape": shape, "cpu_fps": cpu_fps, "gpu_fps": gpu_fps})
 
