ca86a356e735ecace3f67ad31ab4edcaa442b50a
Alex Parinov <creafz@gmail.com>
2021-01-31 09:50:31 +0300

    Add support for Python 3.9 to CI (#830)

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
