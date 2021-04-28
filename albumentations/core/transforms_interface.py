d2afd35802cc2ee1e7392ed941fb3d87fcd80ac0
Dipet <dipetm@gmail.com>
2021-04-28 12:43:48 +0300

    Use the more general type `Sequence` inside `to_tuple` checks

--- albumentations/core/transforms_interface.py
+++ albumentations/core/transforms_interface.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import random
 from warnings import warn
+from typing import Sequence
 
 import cv2
 from copy import deepcopy
@@ -33,7 +34,7 @@ def to_tuple(param, low=None, bias=None):
             param = -param, +param
         else:
             param = (low, param) if low < param else (param, low)
-    elif isinstance(param, (list, tuple)):
+    elif isinstance(param, Sequence):
         param = tuple(param)
     else:
         raise ValueError("Argument param must be either scalar (int, float) or tuple")
