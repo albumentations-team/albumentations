9ce11781a82f0e9fa3decd150d8d97badf966d24
Alex Parinov <creafz@gmail.com>
2021-05-24 18:35:21 +0300

    Fix imgaug import errors

--- albumentations/__init__.py
+++ albumentations/__init__.py
@@ -6,4 +6,10 @@ from .core.composition import *
 from .core.transforms_interface import *
 from .core.serialization import *
 from .augmentations import *
-from .imgaug.transforms import *
+
+try:
+    from .imgaug.transforms import *
+except ImportError:
+    # ImgAug is not installed by default, so we ignore this error.
+    # Run `pip install -U albumentations[imgaug] if you need augmentations from imgaug.`
+    pass
