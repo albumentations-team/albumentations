9560de52d447df1eb910cfd37cffe0f713351a74
Ruan Comelli <ruancomelli@gmail.com>
2021-07-06 12:54:44 -0300

    Fix typo in BboxParams docstring
    An opening backtick was missing in the description of the Albumentations format.

--- albumentations/core/composition.py
+++ albumentations/core/composition.py
@@ -453,7 +453,7 @@ class BboxParams(Params):
                 `[x_min, y_min, x_max, y_max]`, e.g. [97, 12, 247, 212].
             The `albumentations` format
                 is like `pascal_voc`, but normalized,
-                in other words: [x_min, y_min, x_max, y_max]`, e.g. [0.2, 0.3, 0.4, 0.5].
+                in other words: `[x_min, y_min, x_max, y_max]`, e.g. [0.2, 0.3, 0.4, 0.5].
             The `yolo` format
                 `[x, y, width, height]`, e.g. [0.1, 0.2, 0.3, 0.4];
                 `x`, `y` - normalized bbox center; `width`, `height` - normalized bbox width and height.
