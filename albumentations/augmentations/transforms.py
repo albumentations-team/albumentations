8e2d2bb642281e104ef3c69a09bb7ca4c68e2b05
shyn <imvvqX@gmail.com>
2021-10-22 14:47:08 +0800

    W293 fix
    fix W293 blank line contains whitespace

--- albumentations/augmentations/transforms.py
+++ albumentations/augmentations/transforms.py
@@ -2603,7 +2603,7 @@ class MaskDropout(DualTransform):
     Image can be any number of channels.
 
     Inspired by https://www.kaggle.com/c/severstal-steel-defect-detection/discussion/114254
-    
+
     Args:
         max_objects: Maximum number of labels that can be zeroed out. Can be tuple, in this case it's [min, max]
         image_fill_value: Fill value to use when filling image.
