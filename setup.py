348528bf9597994c9117f5cd813445577d24d3f0
Vladimir Iglovikov <iglovikov@gmail.com>
2024-02-27 18:13:59 -0800

    Fix in requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import re
 from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ["numpy>=1.24.4", "scipy>=1.10.0", "scikit-image>=0.21.0", "PyYAML", "typing-extensions>=4.9.0"]
+INSTALL_REQUIRES = ["numpy>=1.24.4", "scipy>=1.10.0", "scikit-image>=0.21.0", "PyYAML", "typing-extensions>=4.9.0", "scikit-learn>=1.3.2"]
 
 # If none of packages in first installed, install second package
 CHOOSE_INSTALL_REQUIRES = [
