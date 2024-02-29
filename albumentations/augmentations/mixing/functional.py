72242069268be26c6aad41ff997c45af3aa093e4
Vladimir Iglovikov <iglovikov@gmail.com>
2024-02-29 08:38:30 -0800

    Fixes from Code Review

--- albumentations/augmentations/mixing/functional.py
+++ albumentations/augmentations/mixing/functional.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
 
-from albumentations.augmentations.utils import clipped
+from albumentations.augmentations.utils import clipped, preserve_shape
 
 
 @clipped
+@preserve_shape
 def mix_arrays(array1: np.ndarray, array2: np.ndarray, mix_coef: float) -> np.ndarray:
-    return cv2.addWeighted(array1, mix_coef, array2.reshape(array1.shape), 1 - mix_coef, 0)
+    array2 = array2.reshape(array1.shape).astype(array1.dtype)
+    return cv2.addWeighted(array1, mix_coef, array2, 1 - mix_coef, 0)
