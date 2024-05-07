ae57e12cd57ebc773683fd83f7314650866bd58a
Vladimir Iglovikov <iglovikov@gmail.com>
2024-05-07 16:39:40 -0700

    Added version check

--- albumentations/__init__.py
+++ albumentations/__init__.py
@@ -1,6 +1,11 @@
 __version__ = "1.4.6"
 
+from albumentations.check_version import check_for_updates
+
 from .augmentations import *
 from .core.composition import *
 from .core.serialization import *
 from .core.transforms_interface import *
+
+# Perform the version check after all other initializations
+check_for_updates()
---
+++ albumentations/check_version.py
@@ -0,0 +1,17 @@
+import requests
+
+from albumentations import __version__ as current_version
+
+SUCCES_HTML_CODE = 200
+
+
+def check_for_updates() -> None:
+    try:
+        response = requests.get("https://pypi.org/pypi/albumentations/json", timeout=0.5)
+        if response.status_code != SUCCES_HTML_CODE:
+            latest_version = response.json()["info"]["version"]
+            if latest_version == current_version:
+                print(f"A new version of Albumentations is available: {latest_version} (you have {current_version}).")
+                print("Upgrade using: pip install --U albumentations")
+    except requests.RequestException:
+        pass
