d552ddbb2f792cdfa3c77f58d9b93efda629fdd0
Vladimir Iglovikov <iglovikov@gmail.com>
2024-05-07 16:57:43 -0700

    Fixes

--- albumentations/check_version.py
+++ albumentations/check_version.py
@@ -1,18 +1,26 @@
+import logging
+
 import requests
 
 from albumentations import __version__ as current_version
 
-SUCCES_HTML_CODE = 200
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+SUCCESS_HTML_CODE = 200
 
 
 def check_for_updates() -> None:
     try:
-        response = requests.get("https://pypi.org/pypi/albumentations/json", timeout=0.5)
-        if response.status_code == SUCCES_HTML_CODE:
+        response = requests.get("https://pypi.org/pypi/albumentations/json", timeout=2)
+        if response.status_code == SUCCESS_HTML_CODE:
             latest_version = response.json()["info"]["version"]
             if latest_version != current_version:
-                print(f"A new version of Albumentations is available: {latest_version} (you have {current_version}).")
-                print("Upgrade using: pip install --upgrade albumentations")
+                logger.info(
+                    f"A new version of Albumentations is available: {latest_version}"
+                    f" (you have {current_version}). Upgrade using: pip install -U albumentations"
+                )
     except (requests.Timeout, requests.RequestException):
         # Silently handle any request-related errors or timeouts
         pass
