39298604c9a98dd75dcbe4d9ac78e8901a3575fc
Vladimir Iglovikov <iglovikov@gmail.com>
2024-05-07 17:04:26 -0700

    Fix

--- albumentations/check_version.py
+++ albumentations/check_version.py
@@ -1,11 +1,11 @@
+import json
 import logging
-
-import requests
+import urllib.request
 
 from albumentations import __version__ as current_version
 
+# Configure logging
 logging.basicConfig(level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 SUCCESS_HTML_CODE = 200
@@ -13,14 +13,25 @@ SUCCESS_HTML_CODE = 200
 
 def check_for_updates() -> None:
     try:
-        response = requests.get("https://pypi.org/pypi/albumentations/json", timeout=2)
-        if response.status_code == SUCCESS_HTML_CODE:
-            latest_version = response.json()["info"]["version"]
-            if latest_version != current_version:
-                logger.info(
-                    f"A new version of Albumentations is available: {latest_version}"
-                    f" (you have {current_version}). Upgrade using: pip install -U albumentations"
-                )
-    except (requests.Timeout, requests.RequestException):
-        # Silently handle any request-related errors or timeouts
+        # Create an OpenerDirector that only allows HTTP and HTTPS
+        opener = urllib.request.build_opener(urllib.request.HTTPHandler(), urllib.request.HTTPSHandler())
+        url = "https://pypi.org/pypi/albumentations/json"
+
+        # Open the URL using the restricted opener
+        with opener.open(url, timeout=2) as response:
+            if response.status == SUCCESS_HTML_CODE:
+                data = response.read()
+                encoding = response.info().get_content_charset("utf-8")
+                json_data = json.loads(data.decode(encoding))
+                latest_version = json_data["info"]["version"]
+                if latest_version != current_version:
+                    logger.info(
+                        f"A new version of Albumentations is available: {latest_version} (you have {current_version})."
+                        " Upgrade using: pip install --upgrade albumentations"
+                    )
+    except urllib.error.URLError:
+        # Silently handle URL errors
+        pass
+    except urllib.error.HTTPError:
+        # Silently handle HTTP errors
         pass
