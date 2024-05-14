10f3d861b07cfcfb59713ebe6bb08641f41fb0b5
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2024-05-14 14:52:45 -0700

    Fixes (#1725)
    * Fixes
    
    * sourcery hints

--- albumentations/check_version.py
+++ albumentations/check_version.py
@@ -1,37 +1,66 @@
 import json
 import logging
 import urllib.request
+from urllib.request import OpenerDirector
 
 from albumentations import __version__ as current_version
 
-# Configure logging
+# Set up basic logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 SUCCESS_HTML_CODE = 200
 
+opener = None
+
+
+def get_opener() -> OpenerDirector:
+    global opener  # noqa: PLW0603
+    if opener is None:
+        opener = urllib.request.build_opener(urllib.request.HTTPHandler(), urllib.request.HTTPSHandler())
+    return opener
+
+
+def fetch_version_info() -> str:
+    logger.debug("Starting to fetch version info...")
+    opener = urllib.request.build_opener(urllib.request.HTTPHandler(), urllib.request.HTTPSHandler())
+    url = "https://pypi.org/pypi/albumentations/json"
+    try:
+        with opener.open(url, timeout=2) as response:
+            logger.debug(f"HTTP status: {response.status}")
+            if response.status == SUCCESS_HTML_CODE:
+                data = response.read()
+                logger.debug(f"Raw data: {data}")
+                encoding = response.info().get_content_charset("utf-8")
+                decoded_data = data.decode(encoding)
+                logger.debug(f"Decoded data: {decoded_data}")
+                return decoded_data
+    except Exception:
+        logger.exception("Error fetching version info")
+    return ""
+
+
+def parse_version(data: str) -> str:
+    """Parses the version from the given JSON data."""
+    if data:
+        try:
+            json_data = json.loads(data)
+            # Use .get() to avoid KeyError if 'version' is not present
+            return json_data.get("info", {}).get("version", "")
+        except json.JSONDecodeError:
+            # This will handle malformed JSON data
+            return ""
+    return ""
+
 
 def check_for_updates() -> None:
     try:
-        # Create an OpenerDirector that only allows HTTP and HTTPS
-        opener = urllib.request.build_opener(urllib.request.HTTPHandler(), urllib.request.HTTPSHandler())
-        url = "https://pypi.org/pypi/albumentations/json"
-
-        # Open the URL using the restricted opener
-        with opener.open(url, timeout=2) as response:
-            if response.status == SUCCESS_HTML_CODE:
-                data = response.read()
-                encoding = response.info().get_content_charset("utf-8")
-                json_data = json.loads(data.decode(encoding))
-                latest_version = json_data["info"]["version"]
-                if latest_version != current_version:
-                    logger.info(
-                        f"A new version of Albumentations is available: {latest_version} (you have {current_version})."
-                        " Upgrade using: pip install --upgrade albumentations"
-                    )
-    except urllib.error.URLError:
-        # Silently handle URL errors
-        pass
-    except urllib.error.HTTPError:
-        # Silently handle HTTP errors
-        pass
+        data = fetch_version_info()
+        latest_version = parse_version(data)
+        if latest_version and latest_version != current_version:
+            logger.info(
+                f"A new version of Albumentations is available: {latest_version} (you have {current_version})."
+                " Upgrade using: pip install --upgrade albumentations"
+            )
+    except Exception as e:  # General exception catch to ensure silent failure  # noqa: BLE001
+        logger.info(f"Failed to check for updates due to an unexpected error: {e}")
