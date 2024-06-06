f0b9403b544f7815abee0471b03b77720d6ea8c9
Vladimir Iglovikov <iglovikov@gmail.com>
2024-06-06 15:30:15 -0700

    Ruff fixes

--- albumentations/__init__.py
+++ albumentations/__init__.py
@@ -10,5 +10,5 @@ from .core.serialization import *
 from .core.transforms_interface import *
 
 # Perform the version check after all other initializations
-if os.getenv("NO_ALBUMENTATIONS_UPDATE", '').lower() not in ['true', '1']:
+if os.getenv("NO_ALBUMENTATIONS_UPDATE", "").lower() not in ["true", "1"]:
     check_for_updates()
