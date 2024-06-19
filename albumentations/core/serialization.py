a07f249a5325b934922587eea1012a85ce184a06
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2024-06-19 15:09:51 -0700

    Boosted version (#1804)
    * Boosted version
    
    * Fix
    
    * Added error to setup.py

--- albumentations/core/serialization.py
+++ albumentations/core/serialization.py
@@ -15,7 +15,7 @@ except ImportError:
     yaml_available = False
 
 
-from albumentations import __version__
+from albumentations._version import __version__
 
 __all__ = ["to_dict", "from_dict", "save", "load"]
 
