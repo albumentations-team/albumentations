bc42cc74122bf024ff0729d995372666897aed08
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2024-07-26 16:02:40 -0700

    Version boosted to 1.4.12 (#1858)

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=get_install_requirements(INSTALL_REQUIRES, CHOOSE_INSTALL_REQUIRES),
     extras_require={
         "hub": ["huggingface_hub"],
-        "text": ["pillow", "nltk"]
+        "text": ["pillow"]
     },
     classifiers=[
     "Development Status :: 5 - Production/Stable",
