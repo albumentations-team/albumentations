ff4a6e00108432788925c0f199cc025dfa8d9422
khkim <khkim@allbogdat.com>
2024-08-23 14:32:47 +0900

    Add pytorch import

--- albumentations/__init__.py
+++ albumentations/__init__.py
@@ -7,6 +7,7 @@ from .augmentations import *
 from .core.composition import *
 from .core.serialization import *
 from .core.transforms_interface import *
+from .pytorch import *
 
 # Perform the version check after all other initializations
 if os.getenv("NO_ALBUMENTATIONS_UPDATE", "").lower() not in {"true", "1"}:
