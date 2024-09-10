2f813d6355b3b9acf27be9b55b7a3329898e3334
Vladimir Iglovikov <iglovikov@gmail.com>
2024-09-10 09:41:24 -0700

    Fixed CoarseDropout

--- albumentations/augmentations/dropout/channel_dropout.py
+++ albumentations/augmentations/dropout/channel_dropout.py
@@ -5,12 +5,11 @@ from typing import Any, Mapping
 
 import numpy as np
 from albucore import get_num_channels
-from pydantic import Field
+from pydantic import AfterValidator, Field
 from typing_extensions import Annotated
 
-from albumentations.core.pydantic import OnePlusIntRangeType
+from albumentations.core.pydantic import check_1plus
 from albumentations.core.transforms_interface import BaseTransformInitSchema, ImageOnlyTransform
-from albumentations.core.types import ColorType
 
 from .functional import channel_dropout
 
@@ -20,24 +19,63 @@ MIN_DROPOUT_CHANNEL_LIST_LENGTH = 2
 
 
 class ChannelDropout(ImageOnlyTransform):
-    """Randomly Drop Channels in the input Image.
+    """Randomly drop channels in the input image.
+
+    This transform randomly selects a number of channels to drop from the input image
+    and replaces them with a specified fill value. This can improve model robustness
+    to missing or corrupted channels.
+
+    The technique is conceptually similar to:
+    - Dropout layers in neural networks, which randomly set input units to 0 during training.
+    - CoarseDropout augmentation, which drops out regions in the spatial dimensions of the image.
+
+    However, ChannelDropout operates on the channel dimension, effectively "dropping out"
+    entire color channels or feature maps.
 
     Args:
-        channel_drop_range (int, int): range from which we choose the number of channels to drop.
-        fill_value (int, float): pixel value for the dropped channel.
-        p (float): probability of applying the transform. Default: 0.5.
+        channel_drop_range (tuple[int, int]): Range from which to choose the number
+            of channels to drop. The actual number will be randomly selected from
+            the inclusive range [min, max]. Default: (1, 1).
+        fill_value (float): Pixel value used to fill the dropped channels.
+            Default: 0.
+        p (float): Probability of applying the transform. Must be in the range
+            [0, 1]. Default: 0.5.
+
+    Raises:
+        NotImplementedError: If the input image has only one channel.
+        ValueError: If the upper bound of channel_drop_range is greater than or
+            equal to the number of channels in the input image.
 
     Targets:
         image
 
     Image types:
-        uint8, uint16, unit32, float32
+        uint8, float32
+
+    Example:
+        >>> import numpy as np
+        >>> import albumentations as A
+        >>> image = np.random.randint(0, 256, (100, 100, 3), dtype=np.uint8)
+        >>> transform = A.ChannelDropout(channel_drop_range=(1, 2), fill_value=128, p=1.0)
+        >>> result = transform(image=image)
+        >>> dropped_image = result['image']
+        >>> assert dropped_image.shape == image.shape
+        >>> assert np.any(dropped_image != image)  # Some channels should be different
+
+    Note:
+        - The number of channels to drop is randomly chosen within the specified range.
+        - Channels are randomly selected for dropping.
+        - This transform is not applicable to single-channel (grayscale) images.
+        - The transform will raise an error if it's not possible to drop the specified
+          number of channels (e.g., trying to drop 3 channels from an RGB image).
+        - This augmentation can be particularly useful for training models to be robust
+          against missing or corrupted channel data in multi-spectral or hyperspectral imagery.
 
     """
 
     class InitSchema(BaseTransformInitSchema):
-        channel_drop_range: OnePlusIntRangeType = (1, 1)
-        fill_value: Annotated[ColorType, Field(description="Pixel value for the dropped channel.")]
+        channel_drop_range: Annotated[tuple[int, int], AfterValidator(check_1plus)]
+        fill_value: Annotated[float, Field(description="Pixel value for the dropped channel.")]
 
     def __init__(
         self,
@@ -66,7 +104,7 @@ class ChannelDropout(ImageOnlyTransform):
             msg = "Can not drop all channels in ChannelDropout."
             raise ValueError(msg)
 
-        num_drop_channels = random.randint(self.channel_drop_range[0], self.channel_drop_range[1])
+        num_drop_channels = random.randint(*self.channel_drop_range)
 
         channels_to_drop = random.sample(range(num_channels), k=num_drop_channels)
 
--- tests/test_augmentations.py
+++ tests/test_augmentations.py
@@ -85,7 +85,6 @@ def test_image_only_augmentations_mask_persists(augmentation_cls, params):
             A.TextImage: dict(font_path="./tests/files/LiberationSerif-Bold.ttf")
         },
         except_augmentations={
-            A.CLAHE,
             A.Equalize,
             A.FancyPCA,
             A.FromFloat,
@@ -313,7 +312,6 @@ def test_augmentations_wont_change_input(augmentation_cls, params):
             A.GridElasticDeform: {"num_grid_xy": (10, 10), "magnitude": 10},
         },
         except_augmentations={
-            A.CLAHE,
             A.Equalize,
             A.FancyPCA,
             A.Posterize,
