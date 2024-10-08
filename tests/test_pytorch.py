ef227fdc1a3603cede8f3b3005bfdbabe548902d
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2024-10-07 17:23:35 -0700

    Add tests for contigous masks (#1975)
    * Empty-Commit
    
    * Added extra tests
    
    * Updated test

--- tests/test_pytorch.py
+++ tests/test_pytorch.py
@@ -261,3 +261,15 @@ def test_to_tensor_v2_on_non_contiguous_array_with_horizontal_flip():
     masks = [image[:, :, 0]] * 2
 
     transform(image=image, masks=masks)
+
+def test_to_tensor_v2_on_non_contiguous_array_with_random_rotate90():
+    transforms = A.Compose([
+    A.RandomRotate90(p=1.0),
+    ToTensorV2(),
+    ])
+
+    img = np.random.randint(0, 256, (640, 480, 3)).astype(np.uint8)
+    masks = [np.random.randint(0, 2, (640, 480)).astype(np.uint8) for _ in range(4)]
+    transformed = transforms(image=img, masks=masks)
+    assert transformed["image"].numpy().shape == (3, 640, 480)
+    assert transformed["masks"][0].shape == (640, 480)
