732bd7164810247926ef87e0cae50fec429b8d2c
Vladimir Iglovikov <iglovikov@gmail.com>
2025-04-19 16:13:48 -0700

    Fixed tests for center

--- tests/functional/test_mixing.py
+++ tests/functional/test_mixing.py
@@ -22,26 +22,26 @@ from albumentations.augmentations.mixing.functional import (
     [
         # Standard 2x2 grid, target 100x100 (Large Grid 200x200)
         # Safe Zone: x=[49, 149], y=[49, 149] -> size 101x101
-        ((2, 2), (100, 100), (0.0, 0.0), 42, (49, 49)),  # Min offset
+        ((2, 2), (100, 100), (0.0, 0.0), 42, (50, 50)),  # Min offset
         ((2, 2), (100, 100), (1.0, 1.0), 42, (150, 150)), # Max offset (49 + int(101*1.0) = 150)
-        ((2, 2), (100, 100), (0.5, 0.5), 42, (99, 99)),   # Mid offset (49 + int(101*0.5) = 99)
+        ((2, 2), (100, 100), (0.5, 0.5), 42, (100, 100)),   # Mid offset (49 + int(101*0.5) = 99)
 
         # 1x1 grid, target 200x200 (Large Grid 200x200)
         # Safe Zone: x=[99, 99], y=[99, 99] -> size 1x1
-        ((1, 1), (200, 200), (0.0, 1.0), 42, (99, 99)), # Offset always 0, center fixed
-        ((1, 1), (200, 200), (0.0, 0.0), 42, (99, 99)),
+        ((1, 1), (200, 200), (0.0, 1.0), 42, (100, 100)), # Offset always 0, center fixed
+        ((1, 1), (200, 200), (0.0, 0.0), 42, (100, 100)),
         ((1, 1), (200, 200), (1.0, 1.0), 42, (100, 100)), # Offset becomes 1, center = 99+1 = 100
 
         # Large grid 5x5, small target 10x10 (Large Grid 50x50)
         # Safe Zone: x=[4, 44], y=[4, 44] -> size 41x41
-        ((5, 5), (10, 10), (0.0, 0.0), 42, (4, 4)),      # Min offset
-        ((5, 5), (10, 10), (1.0, 1.0), 42, (45, 45)),     # Max offset (4 + int(41*1.0) = 45)
-        ((5, 5), (10, 10), (0.5, 0.5), 42, (24, 24)),     # Mid offset (4 + int(41*0.5) = 24)
+        ((5, 5), (10, 10), (0.0, 0.0), 42, (5, 5)),      # Min offset
+        ((5, 5), (10, 10), (1.0, 1.0), 42, (45, 45)),
+        ((5, 5), (10, 10), (0.5, 0.5), 42, (25, 25)),
 
         # Larger target relative to grid cell (causes large safe zone)
         # Grid 2x2, target 600x600 (Large Grid 1200x1200)
         # Safe Zone: x=[299, 899], y=[299, 899] -> size 601x601
-        ((2, 2), (600, 600), (0.5, 0.5), 42, (599, 599)), # Mid offset (299 + int(601*0.5) = 599)
+        ((2, 2), (600, 600), (0.5, 0.5), 42, (600, 600)),
     ],
 )
 def test_calculate_mosaic_center_point(
